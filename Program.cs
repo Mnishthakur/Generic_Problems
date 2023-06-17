e09b17c55f277cc9768d2af7017d74aaf2d25134
Mnishthakur <102964888+Mnishthakur@users.noreply.github.com>
2023-06-17 15:44:41 +0530

    [Manish Thakur]: Added 8th program

--- Program.cs
+++ Program.cs
@@ -1,37 +1,62 @@
 using System;
 namespace ProblemsInGenerics;
-class Program
+
+class MinimumFinder<T> where T : IComparable<T>
 {
-    static T FindMinimum<T>(T value1, T value2, T value3) where T : IComparable<T>
+    private T variable1;
+    private T variable2;
+    private T variable3;
+
+    public MinimumFinder(T variable1, T variable2, T variable3)
+    {
+        this.variable1 = variable1;
+        this.variable2 = variable2;
+        this.variable3 = variable3;
+    }
+
+    public T TestMinimum()
+    {
+        return FindMinimum(variable1, variable2, variable3);
+    }
+
+    private static T FindMinimum(T value1, T value2, T value3)
     {
         return (value1.CompareTo(value2) <= 0 && value1.CompareTo(value3) <= 0) ? value1 :
                (value2.CompareTo(value1) <= 0 && value2.CompareTo(value3) <= 0) ? value2 :
                value3;
     }
-
-    static void Main()
+}
+class Program
+{
+    static void Main(string[] args)
     {
-        string a = "Hey";
+        int num1 = 10;
+        int num2 = 5;
+        int num3 = 8;
+
+        MinimumFinder<int> intMinimumFinder = new MinimumFinder<int>(num1, num2, num3);
+        int minimumInt = intMinimumFinder.TestMinimum();
+        Console.WriteLine("The minimum integer value is: " + minimumInt);
+
+
+        string a = "Heyaaaa";
         string b = "Hello";
         string c = "Namaste";
 
-        string minimumstring = FindMinimum(a, b, c);
-        Console.WriteLine("The minimum value is: " + minimumstring);
+        MinimumFinder<string> stringMinimumFinder = new MinimumFinder<string>(a, b, c);
+        string minimumString = stringMinimumFinder.TestMinimum();
+        Console.WriteLine("The minimum string value is: " + minimumString);
 
         float f1 = 2.6f;
         float f2 = 5.8f;
         float f3 = 9.7f;
 
-        float minimumfloat = FindMinimum(f1, f2, f3);
+        MinimumFinder<float> floatMinimumFinder = new MinimumFinder<float>(f1, f2, f3);
+        float minimumfloat = floatMinimumFinder.TestMinimum();
+        Console.WriteLine{ "The minimum float value is :" + minimumfloat);
 
-        Console.WriteLine("The minimum value is: " + minimumfloat);
 
-        int num1 = 10;
-        int num2 = 5;
-        int num3 = 8;
-
-        int minimumnumber = FindMinimum(num1, num2, num3);
-
-        Console.WriteLine("The minimum value is: " + minimumnumber);
+        }
     }
+
 }
